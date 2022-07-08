2d3af81d8d92ca784b096893c7d67ce8937d89cb
Zoz55 <101155960+Zoz55@users.noreply.github.com>
2022-07-08 16:35:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def get_output():
 	if request.method == 'POST':
 		img = request.files['my_image']
 
-		img_path = "This PC/static/" + img.filename	
+		img_path = "Downloads" + img.filename	
 		img.save(img_path)
 
 		p = predict_label(img_path)
