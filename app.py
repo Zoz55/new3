ab20eed0593a2f9ceb4c5c92f53104e9d7c7eddd
Zoz55 <101155960+Zoz55@users.noreply.github.com>
2022-07-12 01:28:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,55 @@
-from flask import *
-from keras.models import load_model
-from keras.preprocessing import image
-import json
+
+from flask import Flask, render_template, request, redirect, flash
+from werkzeug.utils import secure_filename
+from main import getPrediction
 import os
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from flask_cors import CORS
-from PIL import Image
 
-dic = {0 : 'Covid', 1 : 'Healthy', 2 : 'Lung Tumor', 3 : 'Common Pneumonia'}
+UPLOAD_FOLDER = 'static/'
 
-def predict_label(img_path):
-	i = image.load_img(img_path, target_size=(64,64))
-	i = image.img_to_array(i)/255.0
-	i = i.reshape(1, 64,64,3)
-	p = model.predict_classes(i)
-	return dic[p[0]]
+app = Flask(__name__, static_folder="static")
 
-app = Flask(__name__,  static_folder="static")
-CORS(app)
+#Add reference fingerprint. 
+#Cookies travel with a signature that they claim to be legit. 
+#Legitimacy here means that the signature was issued by the owner of the cookie.
+#Others cannot change this cookie as it needs the secret key. 
+#It's used as the key to encrypt the session - which can be stored in a cookie.
+#Cookies should be encrypted if they contain potentially sensitive information.
+app.secret_key = "secret key"
 
-filename1 = 'chest_model_balanced.h5'
-model = keras.models.load_model(filename1)
+#Define the upload folder to save images uploaded by the user. 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# routes
-@app.route("/", methods=['GET', 'POST'])
-def main():
-	return render_template("index.html")
+#Define the route to be home. 
+#The decorator below links the relative route of the URL to the function it is decorating.
+#Here, index function is with '/', our root directory. 
+#Running the app sends us to index.html.
+#Note that render_template means it looks for the file in the templates folder. 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/about")
-def about_page():
-	return "This Project Was Designed By The Students of Faculty of Electronic Engineering - Egypt"
+#Add Post method to the decorator to allow for form submission. 
+@app.route('/', methods=['POST'])
+def submit_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)  #Use this werkzeug method to secure filename. 
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            label = getPrediction(filename)
+            flash(label)
+            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            flash(full_filename)
+            return redirect('/')
 
-@app.route("/submit", methods = ['GET', 'POST'])
-def get_output():
-	if request.method == 'POST':
-		img = request.files['my_image']
-		imagename =  img.filename
-		path = 'static/Fayez/'
-		
-		img_path = os.path.join(path +imagename)
-		im1 = Image.open(img_path) 
-		img.save(img_path)
-		p = predict_label(img_path)
 
-	return render_template("index.html", prediction = p, img_path = img_path)
+if __name__ == "__main__":
+    app.run()
 
-if __name__=='__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+
