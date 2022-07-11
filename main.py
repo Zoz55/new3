73c18d9d126312bcc37140aeaf71d08b31ed508e
Zoz55 <101155960+Zoz55@users.noreply.github.com>
2022-07-12 01:09:46 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+
+
+
+import numpy as np
+from PIL import Image
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import load_model
+
+
+
+def getPrediction(filename):
+    
+    classes = ['Covid', 'Healthy', 
+               'Lung Tumor', 'Common Pneumonia']
+    le = LabelEncoder()
+    le.fit(classes)
+    le.inverse_transform([2])
+    
+    
+    #Load model
+    my_model=load_model("chest_model_balanced.h5")
+    
+    SIZE = 64 #Resize to same size as training images
+    img_path = 'Fayez/'+filename
+    img = np.asarray(Image.open(img_path).resize((SIZE,SIZE)))
+    
+    img = img/255.      #Scale pixel values
+    
+    img = np.expand_dims(img, axis=0)  #Get it tready as input to the network       
+    
+    pred = my_model.predict(img) #Predict                    
+    
+    #Convert prediction to class name
+    pred_class = le.inverse_transform([np.argmax(pred)])[0]
+    print("Diagnosis is:", pred_class)
+    return pred_class
+
+
+#test_prediction =getPrediction('example.jpg')
